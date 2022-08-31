b0a6157c11e3eb43f45f26a05d727ebbde1ec63a
NailGalimov <nailgalimov8@gmail.com>
2022-08-31 09:47:56 +0300

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,16 @@ import 'focus-visible';
 import lazyIMages from './modules/lazyIMages';
 import documenReady from './helpers/documenReady';
 import initModal from './modules/initModal';
-import aboutSlider from './modules/aboutSlider';
 import menu from './modules/menu';
 import anchorLinks from './modules/anchorLInks';
 import blocksReveal from './modules/animatedBlocks';
-import howItWorksFlag from './modules/howItWorksFlag';
 import validation from './modules/validation';
 
 documenReady(() => {
   lazyIMages();
   initModal();
-  aboutSlider();
   menu();
   anchorLinks();
   blocksReveal();
-  howItWorksFlag();
   validation();
 });
